f79869dd0cca91fd66533dd0a35ea1a365e05607
r-uu <roger.wegner@web.de>
2024-10-06 12:50:44 +0200

    config

--- config/docker/database-init/database-init.sql
+++ config/docker/database-init/database-init.sql
@@ -11,15 +11,15 @@ DROP   ROLE     IF EXISTS lib_test;
 CREATE ROLE               lib_test    WITH LOGIN PASSWORD 'lib_test';
 CREATE SCHEMA             lib_test    AUTHORIZATION lib_test;
 
-\connect r_uu;
-
-DROP   DATABASE IF EXISTS lib_staging;
-CREATE DATABASE           lib_staging;
-\connect                  lib_staging;
-DROP   SCHEMA   IF EXISTS lib_staging;
-DROP   ROLE     IF EXISTS lib_staging;
-CREATE ROLE               lib_staging WITH LOGIN PASSWORD 'lib_staging';
-CREATE SCHEMA             lib_staging AUTHORIZATION lib_staging;
+--\connect r_uu;
+--
+--DROP   DATABASE IF EXISTS lib_staging;
+--CREATE DATABASE           lib_staging;
+--\connect                  lib_staging;
+--DROP   SCHEMA   IF EXISTS lib_staging;
+--DROP   ROLE     IF EXISTS lib_staging;
+--CREATE ROLE               lib_staging WITH LOGIN PASSWORD 'lib_staging';
+--CREATE SCHEMA             lib_staging AUTHORIZATION lib_staging;
 
 \connect r_uu;
 
@@ -31,12 +31,30 @@ DROP   ROLE     IF EXISTS app_test;
 CREATE ROLE               app_test    WITH LOGIN PASSWORD 'app_test';
 CREATE SCHEMA             app_test    AUTHORIZATION app_test;
 
+--\connect r_uu;
+--
+--DROP   DATABASE IF EXISTS app_staging;
+--CREATE DATABASE           app_staging;
+--\connect                  app_staging;
+--DROP   SCHEMA   IF EXISTS app_staging;
+--DROP   ROLE     IF EXISTS app_staging;
+--CREATE ROLE               app_staging WITH LOGIN PASSWORD 'app_staging';
+--CREATE SCHEMA             app_staging AUTHORIZATION app_staging;
+
 \connect r_uu;
 
-DROP   DATABASE IF EXISTS app_staging;
-CREATE DATABASE           app_staging;
-\connect                  app_staging;
-DROP   SCHEMA   IF EXISTS app_staging;
-DROP   ROLE     IF EXISTS app_staging;
-CREATE ROLE               app_staging WITH LOGIN PASSWORD 'app_staging';
-CREATE SCHEMA             app_staging AUTHORIZATION app_staging;
+DROP   DATABASE IF EXISTS app_jeeeraaah;
+CREATE DATABASE           app_jeeeraaah;
+\connect                  app_jeeeraaah;
+DROP   SCHEMA   IF EXISTS app_jeeeraaah_test;
+DROP   ROLE     IF EXISTS app_jeeeraaah_test;
+CREATE ROLE               app_jeeeraaah_test WITH LOGIN PASSWORD 'app_jeeeraaah_test';
+CREATE SCHEMA             app_jeeeraaah_test AUTHORIZATION app_jeeeraaah_test;
+DROP   SCHEMA   IF EXISTS app_jeeeraaah_staging;
+DROP   ROLE     IF EXISTS app_jeeeraaah_staging;
+CREATE ROLE               app_jeeeraaah_staging WITH LOGIN PASSWORD 'app_jeeeraaah_staging';
+CREATE SCHEMA             app_jeeeraaah_staging AUTHORIZATION app_jeeeraaah_staging;
+DROP   SCHEMA   IF EXISTS app_jeeeraaah_production;
+DROP   ROLE     IF EXISTS app_jeeeraaah_production;
+CREATE ROLE               app_jeeeraaah_production WITH LOGIN PASSWORD 'app_jeeeraaah_production';
+CREATE SCHEMA             app_jeeeraaah_production AUTHORIZATION app_jeeeraaah_production;
