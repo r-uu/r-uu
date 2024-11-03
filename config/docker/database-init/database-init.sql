0c9d02fbc87dc05e6893812225cee1e58a79bc1d
r-uu <roger.wegner@web.de>
2024-11-03 23:06:42 +0100

    minor config

--- config/docker/database-init/database-init.sql
+++ config/docker/database-init/database-init.sql
@@ -3,43 +3,39 @@
 
 \connect r_uu;
 
-DROP   DATABASE IF EXISTS lib_test;
-CREATE DATABASE           lib_test;
-\connect                  lib_test;
+DROP   DATABASE IF EXISTS lib;
+CREATE DATABASE           lib;
+\connect                  lib;
 DROP   SCHEMA   IF EXISTS lib_test;
 DROP   ROLE     IF EXISTS lib_test;
-CREATE ROLE               lib_test    WITH LOGIN PASSWORD 'lib_test';
-CREATE SCHEMA             lib_test    AUTHORIZATION lib_test;
-
---\connect r_uu;
---
---DROP   DATABASE IF EXISTS lib_staging;
---CREATE DATABASE           lib_staging;
---\connect                  lib_staging;
+CREATE ROLE               lib_test WITH LOGIN PASSWORD 'lib_test';
+CREATE SCHEMA             lib_test AUTHORIZATION lib_test;
 --DROP   SCHEMA   IF EXISTS lib_staging;
 --DROP   ROLE     IF EXISTS lib_staging;
 --CREATE ROLE               lib_staging WITH LOGIN PASSWORD 'lib_staging';
 --CREATE SCHEMA             lib_staging AUTHORIZATION lib_staging;
+--DROP   SCHEMA   IF EXISTS lib_production;
+--DROP   ROLE     IF EXISTS lib_production;
+--CREATE ROLE               lib_production WITH LOGIN PASSWORD 'lib_production';
+--CREATE SCHEMA             lib_production AUTHORIZATION lib_production;
 
 \connect r_uu;
 
-DROP   DATABASE IF EXISTS app_test;
-CREATE DATABASE           app_test;
-\connect                  app_test;
-DROP   SCHEMA   IF EXISTS app_test;
-DROP   ROLE     IF EXISTS app_test;
-CREATE ROLE               app_test    WITH LOGIN PASSWORD 'app_test';
-CREATE SCHEMA             app_test    AUTHORIZATION app_test;
-
---\connect r_uu;
---
---DROP   DATABASE IF EXISTS app_staging;
---CREATE DATABASE           app_staging;
---\connect                  app_staging;
---DROP   SCHEMA   IF EXISTS app_staging;
---DROP   ROLE     IF EXISTS app_staging;
---CREATE ROLE               app_staging WITH LOGIN PASSWORD 'app_staging';
---CREATE SCHEMA             app_staging AUTHORIZATION app_staging;
+DROP   DATABASE IF EXISTS app_demo;
+CREATE DATABASE           app_demo;
+\connect                  app_demo;
+DROP   SCHEMA   IF EXISTS app_demo_test;
+DROP   ROLE     IF EXISTS app_demo_test;
+CREATE ROLE               app_demo_test WITH LOGIN PASSWORD 'app_demo_test';
+CREATE SCHEMA             app_demo_test AUTHORIZATION app_demo_test;
+--DROP   SCHEMA   IF EXISTS app_demo_staging;
+--DROP   ROLE     IF EXISTS app_demo_staging;
+--CREATE ROLE               app_demo_staging WITH LOGIN PASSWORD 'app_demo_staging';
+--CREATE SCHEMA             app_demo_staging AUTHORIZATION app_demo_staging;
+--DROP   SCHEMA   IF EXISTS app_demo_production;
+--DROP   ROLE     IF EXISTS app_demo_production;
+--CREATE ROLE               app_demo_production WITH LOGIN PASSWORD 'app_demo_production';
+--CREATE SCHEMA             app_demo_production AUTHORIZATION app_demo_production;
 
 \connect r_uu;
 
@@ -50,11 +46,11 @@ DROP   SCHEMA   IF EXISTS app_jeeeraaah_test;
 DROP   ROLE     IF EXISTS app_jeeeraaah_test;
 CREATE ROLE               app_jeeeraaah_test WITH LOGIN PASSWORD 'app_jeeeraaah_test';
 CREATE SCHEMA             app_jeeeraaah_test AUTHORIZATION app_jeeeraaah_test;
-DROP   SCHEMA   IF EXISTS app_jeeeraaah_staging;
-DROP   ROLE     IF EXISTS app_jeeeraaah_staging;
-CREATE ROLE               app_jeeeraaah_staging WITH LOGIN PASSWORD 'app_jeeeraaah_staging';
-CREATE SCHEMA             app_jeeeraaah_staging AUTHORIZATION app_jeeeraaah_staging;
-DROP   SCHEMA   IF EXISTS app_jeeeraaah_production;
-DROP   ROLE     IF EXISTS app_jeeeraaah_production;
-CREATE ROLE               app_jeeeraaah_production WITH LOGIN PASSWORD 'app_jeeeraaah_production';
-CREATE SCHEMA             app_jeeeraaah_production AUTHORIZATION app_jeeeraaah_production;
+--DROP   SCHEMA   IF EXISTS app_jeeeraaah_staging;
+--DROP   ROLE     IF EXISTS app_jeeeraaah_staging;
+--CREATE ROLE               app_jeeeraaah_staging WITH LOGIN PASSWORD 'app_jeeeraaah_staging';
+--CREATE SCHEMA             app_jeeeraaah_staging AUTHORIZATION app_jeeeraaah_staging;
+--DROP   SCHEMA   IF EXISTS app_jeeeraaah_production;
+--DROP   ROLE     IF EXISTS app_jeeeraaah_production;
+--CREATE ROLE               app_jeeeraaah_production WITH LOGIN PASSWORD 'app_jeeeraaah_production';
+--CREATE SCHEMA             app_jeeeraaah_production AUTHORIZATION app_jeeeraaah_production;
